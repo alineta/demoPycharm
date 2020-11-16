982320a2e2214b1ec6527707adf61d5b234f8a4d
rbizoi <razvan.bizoi@gmail.com>
2020-11-16 15:08:37 +0100

    Initial commit

---
+++ fichierPrincipal.py
@@ -0,0 +1,4 @@
+import lireOracle
+
+if __name__ == '__main__':
+    print(lireOracle.donnees.head())
---
+++ lireOracle.py
@@ -0,0 +1,5 @@
+
+import pandas as pd
+
+
+donnees = pd.read_csv('c:/Solutions/donnees/AirQualityUCI01.csv')
