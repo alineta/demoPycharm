2d72e7cb56a163c69de4bf2184e23cc566c5762b
rbizoi <razvan.bizoi@gmail.com>
2020-11-17 14:57:17 +0100

    La mise à jouor

--- fichierPrincipal.py
+++ fichierPrincipal.py
@@ -1,23 +1,7 @@
-import dash_core_components as dcc
-import dash_html_components as html
-
 import lireOracle
-import pandas as pd
-import sqlalchemy
-
 
 if __name__ == '__main__':
-    engine = sqlalchemy.create_engine("oracle+cx_oracle://stag01:Phoenix#Icar67@51.91.76.248:15440/coursdb")
-    print("connecting with engine " + str(engine))
-    connection = engine.connect()
-    query = """
-                select p.table_name,
-                       f.table_name, 
-                       p.constraint_name,
-                       f.constraint_name
-                from user_constraints p join user_constraints f 
-                on f.r_constraint_name = p.constraint_name
-    """
-    df = pd.read_sql_query(query, connection)  # df.to_sql('nom', con=engine,if_exists='replace')
-
+    df = lireOracle.lireConstraintes()
     print(df.head())
+
+    print(lireOracle.lireFichierAir().head())
--- lireOracle.py
+++ lireOracle.py
@@ -1,8 +1,23 @@
-
 import pandas as pd
+import sqlalchemy
 
 def lireFichierAir():
     return pd.read_csv('c:/Solutions/donnees/AirQualityUCI01.csv')
+
+def lireConstraintes():
+    engine = sqlalchemy.create_engine("oracle+cx_oracle://stag01:Phoenix#Icar67@51.91.76.248:15440/coursdb")
+    print("connecting with engine " + str(engine))
+    connection = engine.connect()
+    query = """
+                select p.table_name,
+                       f.table_name, 
+                       p.constraint_name,
+                       f.constraint_name
+                from user_constraints p join user_constraints f 
+                on f.r_constraint_name = p.constraint_name
+    """
+    return pd.read_sql_query(query, connection)
+
 
 
 if __name__ == '__main__':
