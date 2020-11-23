1948d9c5dc4e6680fad0f53f25d8a7668f2d5afc
rbizoi <razvan.bizoi@gmail.com>
2020-11-23 13:32:08 +0100

    La mise à jouor

--- exemples.py
+++ exemples.py
@@ -1,7 +1,10 @@
 import dash
 import dash_cytoscape as cyto
 import dash_html_components as html
+import dash_core_components as dcc
+from dash.dependencies import Input, Output
 from lireOracle import lireOracle
+
 
 def initListeElements():
     lt = lireOracle()
@@ -14,19 +17,85 @@ def initListeElements():
         listeElements.append({'data': {'source': i['table_parent'], 'target': i['table_enfant']}})
     return listeElements
 
-def executeDash(listeElements):
-    app = dash.Dash(__name__)
 
-    app.layout = html.Div([
-        cyto.Cytoscape(
-            id='lesTables',
-            layout={'name': 'circle', 'radius': 20},
-            elements=listeElements
-        )
-    ])
-    app.run_server(debug=False)
 
+class myDash():
+
+    def __init__ ( self):
+        self.myapp = dash.Dash(__name__)
+        self.listeElements = initListeElements()
+        self . default_stylesheet = [
+                {
+                    'selector': 'node',
+                    'style': {
+                        'background-color': '#BFD7B5',
+                        'label': 'data(label)'
+                    }
+                },
+                {
+                    'selector': 'edge',
+                    'style': {
+                        'line-color': '#A3C4BC'
+                    }
+                }
+            ]
+
+    def initLayout(self):
+        self.myapp.layout = html.Div([
+            html.Div([
+                html.Div(style={'width': '50%', 'display': 'inline'}, children=[
+                    'Edge Color:',
+                    dcc.Input(id='input-line-color', type='text')
+                ]),
+                html.Div(style={'width': '50%', 'display': 'inline'}, children=[
+                    'Node Color:',
+                    dcc.Input(id='input-bg-color', type='text')
+                ])
+            ]),
+            cyto.Cytoscape(
+                id='lesTables',
+                layout={'name': 'circle', 'radius': 20},
+                stylesheet=self.default_stylesheet,
+                style={'width': '100%', 'height': '450px'},
+                elements=self.listeElements
+            )
+        ])
+
+    def initCallbacks(self):
+
+        @self.myapp.callback(Output('lesTables', 'stylesheet'),
+                       Input('input-line-color', 'value'),
+                       Input('input-bg-color', 'value'))
+        def update_stylesheet(line_color, bg_color):
+            if line_color is None:
+                line_color = 'transparent'
+
+            if bg_color is None:
+                bg_color = 'transparent'
+
+            new_styles = [
+                {
+                    'selector': 'node',
+                    'style': {
+                        'background-color': bg_color
+                    }
+                },
+                {
+                    'selector': 'edge',
+                    'style': {
+                        'line-color': line_color
+                    }
+                }
+            ]
+
+            return self.default_stylesheet + new_styles
+
+    def run_server(self, *args, **kwargs):
+        self.myapp.run_server(*args, **kwargs)
 
 if __name__ == '__main__':
-    listeElements = initListeElements()
-    executeDash(listeElements)
+    app = myDash()
+    app.initLayout()
+    app.initCallbacks()
+
+    app.run_server(debug=False)
